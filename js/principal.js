ced65e9791d337ae8a8ef412099a6b4759783b6f
Gustavo Rodrigues <gustavosrodrigues0@gmail.com>
2021-09-08 10:47:21 -0300

    Criação de arquivo externo .js

---
+++ js/principal.js
@@ -0,0 +1,12 @@
+function display(...conteudo) {
+    console.log(conteudo);
+}
+
+display("Fui carregado de um arquivo externo.", "asdfaf");
+
+// querySelector aceita tags, classes e id, na maneira como é usado no CSS
+const titulo = document.querySelector(".titulo");			
+// textContent retorna todo o conteúdo de texto do elemento
+console.log(titulo.textContent);
+// Assim é possível mudar o conteúdo textual de um elemtnto HTML
+titulo.textContent = "Aparecida Nutricionista";
