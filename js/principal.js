c0abd7956a8b917d5c4c107fa0a4d95869f21065
Gustavo Rodrigues <gustavosrodrigues0@gmail.com>
2021-09-09 11:51:08 -0300

    Calculando o IMC do primeiro paciente

--- js/principal.js
+++ js/principal.js
@@ -2,7 +2,19 @@ function display(...conteudo) {
     console.log(conteudo);
 }
 
-display("Fui carregado de um arquivo externo.", "asdfaf");
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var imc = peso / (altura * altura); // 100 / 2.0 x 2.0 = 100 / 4 =>>>>>>> 25
+
+console.log(imc);
+
+display("Fui carregado de um arquivo externo.");
 
 // querySelector aceita tags, classes e id, na maneira como é usado no CSS
 const titulo = document.querySelector(".titulo");			
