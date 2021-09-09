91b0ddc1278def74b3f239455b85033ad4c853d5
Gustavo Rodrigues <gustavosrodrigues0@gmail.com>
2021-09-09 17:31:07 -0300

    Inserindo e validando o IMC ná página

--- js/principal.js
+++ js/principal.js
@@ -10,7 +10,31 @@ var peso = tdPeso.textContent;
 var tdAltura = paciente.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 
-var imc = peso / (altura * altura); // 100 / 2.0 x 2.0 = 100 / 4 =>>>>>>> 25
+var tdImc = paciente.querySelector(".info-imc");
+
+//peso = 800;
+//altura = 4;
+
+pesoValido = true;
+alturaValida = true;
+
+if (peso <= 0 || peso >= 750) {
+    console.log("Peso inválido!");
+    pesoValido = false;
+    tdImc.textContent = "Peso inválido";
+}
+
+if (altura <= 0 || altura >= 3) {
+    console.log("Altura inválida!")
+    alturaValida = false;
+    tdImc.textContent = "Altura inválida";
+}
+
+if (pesoValido && alturaValida) {
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}
+
 
 console.log(imc);
 
