9ce905b54eb9373b4a4fb59c4511c0ebc2b3e2c3
Gustavo Rodrigues <gustavosrodrigues0@gmail.com>
2021-09-22 15:49:41 -0300

    Adicionando a captura de erro na requisição

--- js/buscar-pacientes.js
+++ js/buscar-pacientes.js
@@ -2,17 +2,23 @@ let botaoBuscar = document.querySelector("#buscar-pacientes");
 
 botaoBuscar.addEventListener("click", function(){
     let xhr = new XMLHttpRequest();
-
     xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
 
     xhr.addEventListener("load", function(){
-        let resposta = xhr.responseText;
-        let pacientes = JSON.parse(resposta);
+        let erro = document.querySelector("#erro-ajax");
 
-        pacientes.forEach(function(paciente) {
-            addPacienteTabela(paciente);
-        });        
+        if(xhr.status == 200) {
+            erro.classList.add("invisivel");
+            let resposta = xhr.responseText;
+            let pacientes = JSON.parse(resposta);
+    
+            pacientes.forEach(function(paciente) {
+                addPacienteTabela(paciente);
+            });
+        } else {
+            console.log(`Erro: ${xhr.status}` );                        
+            erro.classList.remove("invisivel");
+        }        
     })
-
     xhr.send();
 })
